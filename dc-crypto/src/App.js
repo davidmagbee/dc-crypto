35d0a57dc9c94e45ca13d3f7601368e343eb5e4f
CRHarding <casey.r.harding@gmai.com>
2020-11-23 15:20:41 -0500

    [App] Initial import and rendering of header in place.

--- dc-crypto/src/App.js
+++ dc-crypto/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import Header from './Components/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>DC Crypto</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
