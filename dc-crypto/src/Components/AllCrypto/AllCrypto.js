513373327f2b70b86fe90e9321b8ce2931e098de
CRHarding <casey.r.harding@gmai.com>
2020-11-24 14:11:46 -0500

    [Components][About]+[AllCrypto]+[Contact]+[Data]+[Favorites]+[IndividualCrypto] Added bare bones components to assist with router testing.

---
+++ dc-crypto/src/Components/AllCrypto/AllCrypto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllCrypto = () => {
+  return (
+    <>
+      <h4>All Crypto</h4>
+    </>
+  )
+}
+
+export default AllCrypto;
---
+++ dc-crypto/src/Components/Favorites/Favorites.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Favorites = () => {
+  return (
+    <>
+      <h4>Favorites</h4>
+    </>
+  )
+}
+
+export default Favorites;
