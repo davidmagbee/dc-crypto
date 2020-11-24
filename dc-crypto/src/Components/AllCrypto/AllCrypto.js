4491eea074e0483ca79b983eb076bd5383348a52
CRHarding <casey.r.harding@gmai.com>
2020-11-24 15:06:13 -0500

    [Components][AllCrypto] Dummy api call to api_helper in place.

--- dc-crypto/src/Components/AllCrypto/AllCrypto.js
+++ dc-crypto/src/Components/AllCrypto/AllCrypto.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { getAllStocks } from '../../services/api-helper';
 
 const AllCrypto = () => {
+
+  const getAllData = async() => {
+    const data = await getAllStocks();
+  }
+  getAllData();
   return (
     <>
       <h4>All Crypto</h4>
