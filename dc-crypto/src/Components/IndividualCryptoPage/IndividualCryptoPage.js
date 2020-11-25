5359803f48c6cebcf99e2234f039cc5bd19e8f6a
CRHarding <casey.r.harding@gmai.com>
2020-11-25 11:46:45 -0500

    [Components][IndividualCrypto] Refactored IndividualCrypto layout. Previously we only had one component, now the logic is spread between two. IndividualCryptoItem is used by AllCrypto to list out the individual Crypto items retrieved from the API. On click of an individual item the user is now routed to /crypto/:id which renders the IndividualCryptoPage component.

---
+++ dc-crypto/src/Components/IndividualCryptoPage/IndividualCryptoPage.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { getOneStock } from '../../services/api-helper';
+
+const IndividualCryptoPage = (props) => {
+  const [quote, setQuote] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getOneStock(props.id);
+        setQuote(data);
+      } catch(e) { console.log(e) }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {quote.data
+        ?
+          <div>
+            <p>{quote.data[0].asset_id}</p>
+            <p>Price in USD: {quote.data[0].price_usd}</p>
+            {quote.data[0].type_is_crypto
+              ? <p>Is crypto</p>
+              : <p>Is not crypto</p>}
+          </div>
+        : <p>Data coming soon</p>
+      }
+    </>
+  )
+}
+
+export default IndividualCryptoPage;
