b949a7378fdee0710a5ffb512130840243cd4863
CRHarding <casey.r.harding@gmai.com>
2020-12-06 13:25:54 -0500

    [Components][Favorites] Initial stub for the favorites page in place.

--- dc-crypto/src/Components/Favorites/Favorites.js
+++ dc-crypto/src/Components/Favorites/Favorites.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const Favorites = () => {
+import { Link } from 'react-router-dom';
+
+const Favorites = (props) => {
   return (
     <>
       <h4>Favorites</h4>
+      {props.user && <h5>{props.user.name}</h5>}
+      {props.user && props.user.favorites && (
+        <>
+          {props.user.favorites.map((favorite, key) => {
+            return (
+              <div className="favorites-wrapper">
+                <Link
+                  to={`/crypto/${favorite.currency.asset_id_base}`}
+                >
+                  <h4>{favorite.currency.asset_id_base}</h4>
+                </Link>
+              </div>
+            )
+          })}
+        </>
+      )}
     </>
   )
 }
