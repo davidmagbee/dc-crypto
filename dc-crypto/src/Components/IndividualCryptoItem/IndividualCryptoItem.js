06877bc5467136dd5b7d5185e5167e96c14441e5
CRHarding <casey.r.harding@gmai.com>
2020-12-06 19:56:08 -0500

    [Components][Individual Crypto Item] Added a defensive check to make sure props.currency exists before digging in.

--- dc-crypto/src/Components/IndividualCryptoItem/IndividualCryptoItem.js
+++ dc-crypto/src/Components/IndividualCryptoItem/IndividualCryptoItem.js
@@ -5,12 +5,18 @@ import './IndividualCryptoItem.css';
 const IndividualCryptoItem = (props) => {
   return (
     <div className="item-wrapper">
-      <Link to={`/crypto/${props.currency}`}>
-        <h4>{props.currency}</h4>
-      </Link>
-      <button
-        onClick={() => props.addFavorite(props.currency)}
-      >Add Favorite</button>
+      {props.currency && (
+        <>
+          <Link to={`/crypto/${props.currency}`}>
+            <h4>{props.currency}</h4>
+          </Link>
+          {!props.favorites.includes(props.currency) &&
+            <button
+              onClick={() => props.addFavorite(props.currency)}
+            >Add Favorite</button>
+          }
+        </>      
+      )}
     </div>
   )
 }
