dc44343432dabd35d709fbd737d417e2bcbc5434
CRHarding <casey.r.harding@gmai.com>
2020-12-06 19:57:44 -0500

    [App] Removed a forgotten console.log.

--- dc-crypto/src/App.js
+++ dc-crypto/src/App.js
@@ -14,7 +14,7 @@ import Footer from './Components/Footer/Footer';
 
 function App() {
   const [currentUser, setCurrentUser] = useState({name: "", favorites: []});
-  console.log(currentUser);
+
   // Currently mocking a signed in user
   useEffect(() => {
     const userData = {
